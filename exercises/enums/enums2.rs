197ec814604f7fe2a804efcc37e2f8b39346fa22
AshinZ <nuaaxz18@gmail.com>
2022-11-09 22:05:07 +0800

    feat: finish enums

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -1,11 +1,18 @@
 // enums2.rs
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[derive(Debug)]
+
 enum Message {
     // TODO: define the different variants used below
+    Move{
+        x: u32,
+        y: u32,
+    },
+    Echo(String),
+    ChangeColor(u8,u8,u8),
+    Quit,
 }
 
 impl Message {
