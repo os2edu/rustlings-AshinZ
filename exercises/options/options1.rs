884499a78aa2df1a74c391e6c0cae3dc5529d782
AshinZ <nuaaxz18@gmail.com>
2022-11-10 22:47:47 +0800

    feat: finish options

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -1,7 +1,8 @@
 // options1.rs
 // Execute `rustlings hint options1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+use std::time;
 
 // This function returns how much icecream there is left in the fridge.
 // If it's before 10PM, there's 5 pieces left. At 10PM, someone eats them
@@ -10,7 +11,13 @@
 fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // We use the 24-hour system here, so 10PM is a value of 22 and 12AM is a value of 0
     // The Option output should gracefully handle cases where time_of_day > 23.
-    ???
+    if time_of_day < 22 {
+        Option::Some(5)
+    } else if time_of_day >23{
+        Option::None
+    } else {
+        Option::Some(0)
+    }
 }
 
 #[cfg(test)]
@@ -30,6 +37,15 @@ mod tests {
     fn raw_value() {
         // TODO: Fix this test. How do you get at the value contained in the Option?
         let icecreams = maybe_icecream(12);
-        assert_eq!(icecreams, 5);
+        let mut a:u16 = 0;
+        match icecreams{
+            Option::None=>{
+                a = 0
+            },
+            Option::Some(num)=>{
+                a = num
+            },
+        }
+        assert_eq!(a, 5);
     }
 }
